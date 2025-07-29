c76691d470fae8e80f3d5b69d74bc241fec0cd6b
hengshengyong <3271660881@qq.com>
2025-07-29 10:57:17 +0800

    MessageDialog 组件

---
+++ src/MessageDialog/index.tsx
@@ -0,0 +1,137 @@
+// @ts-ignore
+import MessageDialog from 'miot/ui/Dialog/MessageDialog';
+import React, { useState } from 'react';
+import { Button } from 'react-native';
+
+const App: React.FC<any> = () => {
+  const [visible4, setVisible4] = useState(false);
+  const [visible5, setVisible5] = useState(false);
+  const [visible6, setVisible6] = useState(false);
+  const [visible7, setVisible7] = useState(false);
+
+  const onDismiss = (data: '4' | '5' | '6' | '7') => {
+    console.log('data:', data);
+    visible4 && setVisible4(false);
+    visible5 && setVisible5(false);
+    visible6 && setVisible6(false);
+    visible7 && setVisible7(false);
+  };
+
+  return (
+    <>
+      <Button
+        onPress={() => {
+          console.log('无标题');
+          setVisible4(!visible4);
+        }}
+        title="无标题"
+      />
+      <MessageDialog
+        visible={visible4}
+        buttons={[
+          {
+            text: '我了解了',
+            style: { color: 'lightpink' },
+            callback: (_: any) => setVisible4(false),
+          },
+        ]}
+        onDismiss={(_: any) => onDismiss('4')}
+      />
+      <Button
+        onPress={() => {
+          console.log('有标题');
+          setVisible5(!visible5);
+        }}
+        title="有标题"
+      />
+      <MessageDialog
+        visible={visible5}
+        title="消息弹窗自定义标题"
+        message={'消息弹窗自定义标题'}
+        buttons={[
+          {
+            text: '消失',
+            style: { color: 'lightpink' },
+            callback: (_: any) => setVisible5(false),
+          },
+          {
+            text: '不消失',
+            style: { color: 'lightblue' },
+            callback: (_: any) => console.log('不消失'),
+          },
+        ]}
+        onDismiss={(_: any) => onDismiss('5')}
+      />
+      <Button
+        onPress={() => {
+          console.log('带下划线');
+          setVisible6(!visible6);
+        }}
+        title="带下划线"
+      />
+      <MessageDialog
+        type={MessageDialog.TYPE.UNDERLINE}
+        visible={visible6}
+        color="#f0ac3d"
+        title="下划线消息弹窗"
+        message={'下划线消息弹窗'}
+        extraText="你点我一下试试"
+        extra={{
+          onPress: (_: any) => console.log('点击了下划线'),
+        }}
+        buttons={[
+          {
+            text: '取消',
+            style: { color: 'lightpink' },
+            callback: (_: any) => setVisible6(false),
+          },
+          {
+            text: '确认',
+            style: { color: 'lightblue' },
+            callback: (obj: any) => {
+              console.log(`是否点击了下划线: ${obj.hasPressUnderlineText}`);
+              setVisible6(false);
+            },
+          },
+        ]}
+        onDismiss={(_: any) => onDismiss('6')}
+      />
+      <Button
+        onPress={() => {
+          console.log('带勾选框');
+          setVisible7(!visible7);
+        }}
+        title="带勾选框"
+      />
+      <MessageDialog
+        type={MessageDialog.TYPE.CHECKBOX}
+        visible={visible7}
+        color="#f0ac3d"
+        title="勾选框消息弹窗"
+        message={'勾选框消息弹窗'}
+        extraText="快点我试试"
+        extra={{
+          onPress: (_: any) => console.log("点击了'快点我试试'"),
+        }}
+        buttons={[
+          {
+            text: '取消',
+            style: { color: 'lightpink' },
+            callback: (_: any) => setVisible7(false),
+          },
+          {
+            text: '确认',
+            style: { color: 'lightblue' },
+            callback: (obj: any) => {
+              console.log(`是否勾选: ${obj.checked}`);
+              setVisible7(false);
+            },
+          },
+        ]}
+        onDismiss={(_: any) => onDismiss('7')}
+      />
+    </>
+  );
+};
+
+export default App;
