1076b58e69402d11672f6ff14b1195d9440d301a
hengshengyong <3271660881@qq.com>
2025-08-05 10:55:40 +0800

    feat: 文档打包配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,67 @@
-import { defineConfig } from 'dumi';
+//#region dumi 配置
+// import { defineConfig } from 'dumi';
 
-export default defineConfig({
+// export default defineConfig({
+//   title: 'Miot-Design',
+//   favicon: '/assets/Mijia_icon.png',
+//   logo: '/assets/Mijia_icon.png',
+//   outputPath: 'docs-dist',
+//   mode: 'site',
+//   alias: {},
+//   // 单语言配置方式如下
+//   // navs: [
+//   //   null, // null 值代表保留约定式生成的导航，只做增量配置
+//   //   {
+//   //     title: 'GitHub',
+//   //     path: 'https://github.com/umijs/dumi',
+//   //   },
+//   //   {
+//   //     title: '我有二级导航',
+//   //     path: '链接是可选的',
+//   //     // 可通过如下形式嵌套二级导航菜单，目前暂不支持更多层级嵌套：
+//   //     children: [
+//   //       { title: '第一项', path: 'https://d.umijs.org' },
+//   //       { title: '第二项', path: '/guide' },
+//   //     ],
+//   //   },
+//   // ],
+//   devServer: {
+//     port: '8888',
+//   },
+//   theme: {
+//     // 修改 dumi 默认主题的主色，更多变量详见：https://github.com/umijs/dumi/blob/1.x/packages/theme-default/src/style/variables.less
+//     '@c-primary': '#1DA57A',
+//   },
+//   // 如何生效?
+//   themeConfig: {
+//     name: '你好',
+//     prefersColor: { default: 'light', switch: true },
+//     socialLinks: {
+//       github: 'https://github.com/tastien',
+//     },
+//   },
+//   // more config: https://d.umijs.org/config
+// });
+//#endregion
+
+export default {
   title: 'Miot-Design',
-  favicon: '/assets/Mijia_icon.png',
-  logo: '/assets/Mijia_icon.png',
+  favicon: '/assets/images/Mijia_icon.png',
+  logo: '/assets/images/Mijia_icon.png',
   outputPath: 'docs-dist',
   mode: 'site',
+  alias: {},
+  // 配置具体含义见：https://github.com/umijs/umi-webpack-bundle-analyzer#options-for-plugin
+  analyze: {
+    analyzerMode: 'server',
+    analyzerPort: 8889,
+    openAnalyzer: true,
+    // generate stats file while ANALYZE_DUMP exist
+    generateStatsFile: false,
+    statsFilename: 'stats.json',
+    logLevel: 'info',
+    defaultSizes: 'parsed', // stat  // gzip
+  },
   // 单语言配置方式如下
   // navs: [
   //   null, // null 值代表保留约定式生成的导航，只做增量配置
@@ -32,11 +88,11 @@ export default defineConfig({
   },
   // 如何生效?
   themeConfig: {
-    name: '你好',
+    carrier: 'ymyy', // 设备状态栏左侧的文本内容
     prefersColor: { default: 'light', switch: true },
     socialLinks: {
       github: 'https://github.com/tastien',
     },
   },
   // more config: https://d.umijs.org/config
-});
+};
